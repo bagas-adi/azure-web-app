a213bc330eca272ea7b008d291135289d3ddf7bc
LAPTOP-9VPQ2I5R\lenovo <bagasap90@gmail.com>
2019-04-05 19:11:59 +0700

    connect sql server 10

--- index.php
+++ index.php
@@ -50,6 +50,10 @@
               echo ("Reading data from table" . PHP_EOL);
               if ($getResults == FALSE)
                   echo (sqlsrv_errors());
+              while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+               echo ($row['ID'] . " " . $row['Name'] . PHP_EOL);
+              };
+              sqlsrv_free_stmt($getResults);
             } catch (Exception $e){
               echo "Failed: " . $e;
             };
