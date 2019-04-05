043d35cdf87f3a74f978c0b298e1c0b353d7afb6
LAPTOP-9VPQ2I5R\lenovo <bagasap90@gmail.com>
2019-04-05 17:34:46 +0700

    tes 5 koneksi ke SQL Server di index

--- index.php
+++ index.php
@@ -21,24 +21,24 @@
           <h4>
             <?php 
             // PHP Data Objects(PDO) Sample Code:
-            // try {
-            //     $conn = new PDO("sqlsrv:server = tcp:bagasap90.database.windows.net,1433; Database = bagasap90-db", "bagaswtf", "Bagasadi90--");
-            //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // }
-            // catch (PDOException $e) {
-            //     print("Error connecting to SQL Server.");
-            //     die(print_r($e));
-            // }
-
-            // SQL Server Extension Sample Code:
-            $connectionInfo = array("UID" => "bagaswtf@bagasap90", "pwd" => "Bagasadi90--", "Database" => "bagasap90-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-            $serverName = "tcp:bagasap90.database.windows.net,1433";
-            $conn = sqlsrv_connect($serverName, $connectionInfo);
-            if ($conn) {
-              echo "Berhasil konek";
-            } else {
-              echo "Gagal konek";
+            try {
+                $conn = new PDO("sqlsrv:server = tcp:bagasap90.database.windows.net,1433; Database = bagasap90-db", "bagaswtf", "Bagasadi90--");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
+            catch (PDOException $e) {
+                print("Error connecting to SQL Server.");
+                die(print_r($e));
+            };
+
+            // // SQL Server Extension Sample Code:
+            // $connectionInfo = array("UID" => "bagaswtf@bagasap90", "pwd" => "Bagasadi90--", "Database" => "bagasap90-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+            // $serverName = "tcp:bagasap90.database.windows.net,1433";
+            // $conn = sqlsrv_connect($serverName, $connectionInfo);
+            // if ($conn) {
+            //   echo "Berhasil konek";
+            // } else {
+            //   echo "Gagal konek";
+            // };
             ?>
           </h4>
       </div>
