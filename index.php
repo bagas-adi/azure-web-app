89bed6f49cc002c4b7f431837e1e8a58f2b3c22e
LAPTOP-9VPQ2I5R\lenovo <bagasap90@gmail.com>
2019-04-06 20:47:14 +0700

    penambahan form \& show table

--- index.php
+++ index.php
@@ -3,62 +3,92 @@
   include 'koneksi.php';
   $conn = connectDB();
   if($conn === false){
-    echo "false";
-  } else {
-    echo "true";
-    showData($conn);
-    insertDB($conn,"Bagas Adi","bagasap90@mail.com.id","Software developers");
+    //------------------------------------------------------------------ Koneksi ke Database gagal
+    die( print_r( sqlsrv_errors(), true));
+  } else { 
+    // showData($conn);
+    // insertDB($conn,"Bagas Adi","bagasap90@mail.com.id","Software developers");
+    if(isset($_POST['submit'])){
+      $nama = $_POST['nama'];
+      $email = $_POST['email'];
+      $job = $_POST['job'];
+      insertDB($conn,$nama,$email,$job);
+    }
+    //------------------------------------------------------------------ Koneksi ke Database berhasil
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Azure Web App - Bagasap90</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+      </head>
+
+      <body>
+        <div class="navbar-wrapper">
+          <div class="container">
+              <h1>Selamat Datang peserta Microsoft Azure Developer!</h1>
+              <h4>Silahkan isi data diri Anda</h4>
+              <hr/> 
+          </div>
+        </div>
+
+        <!-- Wrap the rest of the page in another container to center all the content. -->
+
+        <div class="container">
+            
+          <div class="row">
+            <div class="col-xs-12 col-sm-6"> 
+              <h2>Registration Form</h2>
+              <form  action="index.php" method="post">
+                 <div class="form-group">
+                  <label for="usr">Name:</label>
+                  <input name="nama" type="text" class="form-control" >
+                </div>
+                 <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input name="email" type="text" class="form-control" >
+                </div>
+                 <div class="form-group">
+                  <label for="job">Pekerjaan:</label>
+                  <input name="job" type="text" class="form-control" >
+                </div> 
+              <!--   <input type="text" name="nama" placeholder="Nama">
+                <input type="text" name="email" placeholder="Email Address">
+                <input type="text" name="job" placeholder="Pekerjaan"> -->
+
+              <div class="form-group">
+                  <input class="btn btn-default" type="submit" name="submit">
+              </div>
+              </form>
+            </div>
+              <div class="col-xs-12 col-sm-6">
+                  <img class="img-rectangle" src="img/cdn.png" alt="CDN" height="140">
+                  <h2>Peserta yang telah mendaftar</h2>
+                  <?php 
+                  showData($conn);
+                  ?>
+              </div>
+          </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="js/bootstrap.min.js"></script>
+      </body>
+    </html>
+
+    <?php
   }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Azure Web App - Bagasap90</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <div class="navbar-wrapper">
-      <div class="container">
-          <h1>Selamat Datang!</h1>
-          <h4>Silahkan isi data diri Anda</h4>
-          <hr/>
-          <h4>
-          </h4>
-      </div>
-    </div>
-
-    <!-- Wrap the rest of the page in another container to center all the content. -->
-
-    <div class="container">
-        
-      <div class="row">
-        <div class="col-xs-12 col-sm-6">
-          <img class="img-rectangle" src="img/azure-portal.png" alt="Azure App Service Web Apps" height="140">
-          <h2>Azure App Service Web Apps</h2>
-          <p>App Service Web Apps is a fully managed compute platform that is optimized for hosting websites and web applications. This platform-as-a-service (PaaS) offering of Microsoft Azure lets you focus on your business logic while Azure takes care of the infrastructure to run and scale your apps.</p>
-        </div>
-          <div class="col-xs-12 col-sm-6">
-              <img class="img-rectangle" src="img/cdn.png" alt="CDN" height="140">
-              <h2>Azure Content Delivery Network (CDN)</h2>
-              <p>The Azure Content Delivery Network (CDN) caches static web content at strategically placed locations to provide maximum throughput for delivering content to users. The CDN offers developers a global solution for delivering high-bandwidth content by caching the content at physical nodes across the world.</p>
-          </div>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
--- koneksi.php
+++ koneksi.php
@@ -2,19 +2,7 @@
 function connectDB(){
   $connectionInfo = array("Database" => "bagasap90-db","UID" => "bagaswtf@bagasap90", "PWD" => "Bagasadi90--", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
   $serverName = "tcp:bagasap90.database.windows.net,1433";
-  return sqlsrv_connect($serverName, $connectionInfo);
-  // try{
-  //   $conn = 
-  //   if( $conn === false ) {
-  //        // die( print_r( sqlsrv_errors(), true));
-  //        return $conn;
-  //   } else {
-  //     return $conn;
-  //   };
-  // } catch (Exception $e){
-  //   // echo "Failed: " . $e;
-  //   return $conn;
-  // };
+  return sqlsrv_connect($serverName, $connectionInfo); 
 };
 function insertDB($conn,$nama,$email,$job){
   $tsql= "INSERT INTO [dbo].[Register] (Nama,Email,Job) VALUES ('".$nama."','".$email."', '".$job."')";
@@ -30,33 +18,13 @@ function showData($conn){
     if( $getResults === false ) {
        die( print_r( sqlsrv_errors(), true));
     } else {
-      // echo ("Reading data from table" . PHP_EOL);
+      echo ("<table>");
+      echo ("<tr><th>ID</th><th>Nama</th><th>Email</th><th>Job</th></tr>");
       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-         echo ($row['ID'] . " " . $row['Nama'] . PHP_EOL);
+         echo ("<tr><td>".$row['ID'] . "</td><td>" . $row['Nama'] . "</td><td>" .$row['Email']. "</td><td>".$row['Job']."</td></tr>");
       };
+      echo ("</table>");
     };
     sqlsrv_free_stmt($getResults);
   };
-// $connectionInfo = array("Database" => "bagasap90-db","UID" => "bagaswtf@bagasap90", "PWD" => "Bagasadi90--", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-// $serverName = "tcp:bagasap90.database.windows.net,1433";
-// try{
-//   $conn = sqlsrv_connect($serverName, $connectionInfo);
-//   if( $conn === false ) {
-//        die( print_r( sqlsrv_errors(), true));
-//   } else {
-//     $tsql= "SELECT * FROM [dbo].[User]";
-//     $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
-//     if( $getResults === false ) {
-//        die( print_r( sqlsrv_errors(), true));
-//     } else {
-//       // echo ("Reading data from table" . PHP_EOL);
-//       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-//          echo ($row['ID'] . " " . $row['Nama'] . PHP_EOL);
-//       };
-//       sqlsrv_free_stmt($getResults);
-//     }
-//   } 
-// } catch (Exception $e){
-//   echo "Failed: " . $e;
-// };
 ?>
