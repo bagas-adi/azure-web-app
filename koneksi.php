76e64ea4c2763c43bb34c270e769abdc992dc123
LAPTOP-9VPQ2I5R\lenovo <bagasap90@gmail.com>
2019-04-06 19:37:15 +0700

    edit index.php 3

--- koneksi.php
+++ koneksi.php
@@ -9,20 +9,20 @@ function connectDB(){
 	       return false;
 	  } else {
 	  	return true;
-	  }
+	  };
 	} catch (Exception $e){
 	  // echo "Failed: " . $e;
 	  return true;
 	};
-}
-function insertDB($nama,$email,$job){
-	$tsql= "INSERT INTO [dbo].[Register] (Nama,Email,Job) VALUES ($nama,$email, $job)";
-    $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
-    if( $getResults === false ) {
-       die( print_r( sqlsrv_errors(), true));
-    } 
-    sqlsrv_free_stmt($getResults);
-} 
+};
+// function insertDB($nama,$email,$job){
+// 	$tsql= "INSERT INTO [dbo].[Register] (Nama,Email,Job) VALUES ($nama,$email, $job)";
+//     $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
+//     if( $getResults === false ) {
+//        die( print_r( sqlsrv_errors(), true));
+//     };
+//     sqlsrv_free_stmt($getResults);
+// };
 function showData(){
 	$tsql= "SELECT * FROM [dbo].[Register]";
     $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
@@ -33,10 +33,10 @@ function showData(){
       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
          echo ($row['ID'] . " " . $row['Nama'] . PHP_EOL);
       };
-    }
+    };
   	sqlsrv_free_stmt($getResults);
-  } 
-}
+  }; 
+};
 // $connectionInfo = array("Database" => "bagasap90-db","UID" => "bagaswtf@bagasap90", "PWD" => "Bagasadi90--", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 // $serverName = "tcp:bagasap90.database.windows.net,1433";
 // try{
