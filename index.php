f959195b1f45766715eced0c6f3e0c212a027d71
LAPTOP-9VPQ2I5R\lenovo <bagasap90@gmail.com>
2019-04-06 19:48:43 +0700

    edit index.php 7

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 
 <?php   
-  function connectDB(){
-  $connectionInfo = array("Database" => "bagasap90-db","UID" => "bagaswtf@bagasap90", "PWD" => "Bagasadi90--", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-  $serverName = "tcp:bagasap90.database.windows.net,1433";
-  try{
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-    if( $conn === false ) {
-         // die( print_r( sqlsrv_errors(), true));
-         return false;
-    } else {
-      return true;
-    };
-  } catch (Exception $e){
-    // echo "Failed: " . $e;
-    return true;
-  };
-};
+  include 'koneksi.php';
 function insertDB($nama,$email,$job){
   $tsql= "INSERT INTO [dbo].[Register] (Nama,Email,Job) VALUES ($nama,$email, $job)";
     $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
@@ -37,7 +22,6 @@ function showData(){
     };
     sqlsrv_free_stmt($getResults);
   }; 
-};
   if(connectDB()){
     echo "true";
   } else {
--- koneksi.php
+++ koneksi.php
@@ -15,28 +15,7 @@ function connectDB(){
     return true;
   };
 };
-function insertDB($nama,$email,$job){
-	$tsql= "INSERT INTO [dbo].[Register] (Nama,Email,Job) VALUES ($nama,$email, $job)";
-    $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
-    if( $getResults === false ) {
-       die( print_r( sqlsrv_errors(), true));
-    };
-    sqlsrv_free_stmt($getResults);
-};
-function showData(){
-	$tsql= "SELECT * FROM [dbo].[Register]";
-    $getResults= sqlsrv_query($conn, $tsql) or die("Error ".sqlsrv_errors());
-    if( $getResults === false ) {
-       die( print_r( sqlsrv_errors(), true));
-    } else {
-      // echo ("Reading data from table" . PHP_EOL);
-      while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-         echo ($row['ID'] . " " . $row['Nama'] . PHP_EOL);
-      };
-    };
-  	sqlsrv_free_stmt($getResults);
-  }; 
-};
+
 // $connectionInfo = array("Database" => "bagasap90-db","UID" => "bagaswtf@bagasap90", "PWD" => "Bagasadi90--", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 // $serverName = "tcp:bagasap90.database.windows.net,1433";
 // try{
